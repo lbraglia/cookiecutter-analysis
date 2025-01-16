00b2fb4bb9ed0f6478c68e1e1d4ba738ae5016ac
Luca Braglia <lbraglia@gmail.com>
2025-01-16 16:15:54 +0100

    update

---
+++ {{ cookiecutter.project_dir }}/src/data_cleaning.py
@@ -0,0 +1,55 @@
+import pylbmisc as lb
+import pandas as pd
+from pprint import pprint
+
+# Data import
+# -----------
+dfs = lb.io.import_data("data/raw_dataset.xlsx")
+
+# Sanitize variable names, keeping as comment
+# -------------------------------------------
+dfs, comments = lb.dm.sanitize_varnames(dfs)
+
+# Unique values inspection/monitoring
+# -----------------------------------
+lb.dm.dump_unique_values(dfs)
+
+    
+# Type coercions: help(lb.dm.Coercer)
+# -----------------------------------
+# # dput variable names
+# for k, df in dfs.items():
+#     print(k)
+#     pprint(df.columns.to_list())
+
+# # custom coercers
+# def _sano(x):
+#     return lb.dm.to_categorical(x, ["Sano", "Paziente"])
+
+# def _numeric_insight(x):
+#     ft = {"Male": 1,
+#           "Non saprei": 2,
+#           "Bene":  3}
+#     return x.map(ft)
+#
+# df_coercions = {
+#     lb.dm.to_sex    : ["sex"],
+#     _sano           : ["sano"]: ,
+#     _numeric_insight: ["insight_punteggi_1"]
+# }
+# 
+# df2_coercions = {
+#     lb.dm.to_sex    : ["sex"],
+#     _sano           : ["sano"]: ,
+#     _numeric_insight: ["insight_punteggi_1"]
+# }
+#
+# # prima di questo passaggio impostare gli indici in dfs per il report dei NA?
+# df  = lb.dm.Coercer(dfs["df"], coercions_df).coerce()
+# df2 = lb.dm.Coercer(dfs["df2"], coercions_df2).coerce()
+
+
+# # Export for analysis
+# # -------------------
+# dfs2 = {"df": df, "df2": df2}
+# lb.io.export_data(dfs2, "tmp/clean", ext = "pkl")
